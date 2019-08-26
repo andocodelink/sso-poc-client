546ac6dba85adb1cffb3c01d7e2fd62a0305c2cc
An Do <an.do@codelink.io>
2019-08-26 15:46:34 +0700

    Redirect to SSO POC login page

--- src/apis/index.ts
+++ src/apis/index.ts
@@ -2,19 +2,17 @@ const API_ROOT = 'http://localhost:3002'
 
 export const greet = async () => {
   const res = await fetch(API_ROOT, {
-    credentials: 'include',
   })
   return res.text()
 }
 
-export const loginWithCodelink = async () => {
+export const login= async () => {
   let loginData = new URLSearchParams("username=admin&password=admin")
   const res = await fetch(API_ROOT + '/login', {
     method: 'POST',
     headers: {
       'Content-Type': 'application/x-www-form-urlencoded;charset=UTF-8'
     },
-    credentials: 'include',
     body: loginData
   })
   if (res.status == 200) {
@@ -25,9 +23,24 @@ export const loginWithCodelink = async () => {
   throw new Error(data.status || 'error');
 }
 
+export const loginWithCodelink = async () => {
+  const res = await fetch(API_ROOT + '/login-sso', {
+    redirect: 'follow',
+    method: 'GET'
+  })
+  if (res.status == 200) {
+    if (res.redirected) {
+      window.open(res.url);
+    }
+    const data = await res.json();
+    return data.access_token;
+  }
+
+  throw new Error(data.status || 'error');
+}
+
 export const getSession = async () => {
   const res = await fetch(API_ROOT + '/login', {
-    credentials: 'include'
   })
   if (res.status == 200) {
     const data = await res.json();
@@ -39,7 +52,6 @@ export const getSession = async () => {
 
 export const logout = async () => {
   const res = await fetch(API_ROOT + '/logout', {
-    credentials: 'include',
   })
   if (res.status == 200) {
     const data = await res.json();
